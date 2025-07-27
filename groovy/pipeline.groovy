1389c6c15e7ec2c9eacd961b5c305e7844336358
Albert Riu <arium@eservicios.indra.es>
2025-07-27 20:01:45 +0200

    Secure sensitive data with ansible vault

--- groovy/pipeline.groovy
+++ groovy/pipeline.groovy
@@ -34,7 +34,7 @@ pipeline{
                 sshagent(['github-sshkey']) {
                     sh """
                         cd fluentbit-tests/ansible 
-                        echo "$VAULT_PASSWORD" > vault_pass.txt
+                        echo "$ANSIBLE_VAULT_PASS" > vault_pass.txt
                         ansible-playbook playbook.yaml --extra-vars namespace=${name} --vault-password-file < vault_pass.txt")
                         rm vault_pass.txt
                         cd ../
